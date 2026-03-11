3e50c4f75a5c6de1fc3b7babcf4b81807ce5fa2d
Abhinav Shakya <136550261+abhinav-123457@users.noreply.github.com>
2026-03-11 23:34:06 +0530

    Update NavLink.tsx

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,28 +1,11 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
+import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { ComponentProps } from "react";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+interface NavLinkProps extends ComponentProps<typeof Link> {
   className?: string;
-  activeClassName?: string;
-  pendingClassName?: string;
 }
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
-    return (
-      <RouterNavLink
-        ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
-        {...props}
-      />
-    );
-  },
-);
-
-NavLink.displayName = "NavLink";
-
-export { NavLink };
+export function NavLink({ className, ...props }: NavLinkProps) {
+  return <Link className={cn(className)} {...props} />;
+}
